0d47d548395e9295dce7f4cb26b1800f86c4f78e
Julien Coquet <julien.coquet@gmail.com>
2019-10-01 18:49:10 +0200

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Stores a matrix and caches its invert
+makeCacheMatrix <- function(x = matrix(c(1:4),2,2)) {
+  # Pass x as "global variable"
+  original_matrix <<- x
+  
+  # Solve the matrix and cache it
+  cached_invert_matrix <<- solve(x)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function() {
+  # Look for original matrix
+  x = original_matrix
+  # Test whether cached matrix exists
+  if (!is.null(cached_invert_matrix)){
+    # The matrix exists, return it
+    message("Getting matrix invert from cache")
+    cached_invert_matrix
+  } else {
+    # solve the matrix
+    message("Solving the matrix")
+    solved_matrix <- solve(x)
+    solved_matrix
+  }
 }
